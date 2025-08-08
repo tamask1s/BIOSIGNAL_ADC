a4d58ebb656efc35c06b489ff084a468671a680c
DaniellWS <allagadaniel98@gmail.com>
2025-08-08 13:20:32 +0200

    minor cleanup

--- src/ads1293.cpp
+++ src/ads1293.cpp
@@ -4,6 +4,66 @@
 #include <SPI.h>
 #include "ads1293.h"
 
+#define CONFIG        0x00
+#define FLEX_CH1_CN   0x01
+#define FLEX_CH2_CN   0x02
+#define FLEX_CH3_CN   0x03
+#define FLEX_PACE_CN  0x04
+#define FLEX_VBAT_CN  0x05
+
+#define LOD_CN        0x06
+#define LOD_EN        0x07
+#define LOD_CURRENT   0x08
+#define LOD_AC_CN     0x09
+
+#define CMDET_EN      0x0a
+#define CMDET_CN      0x0b
+#define RLD_CN        0x0c
+
+#define WILSON_EN1    0x0d
+#define WILSON_EN2    0x0e
+#define WILSON_EN3    0x0f
+#define WILSON_CN     0x10
+
+#define REF_CN        0x11
+#define OSC_CN        0x12
+
+#define AFE_RES       0x13
+#define AFE_SHDN_CN   0x14
+#define AFE_FAULT_CN  0x15
+#define AFE_PACE_CN   0x17
+
+#define ERROR_LOD     0x18
+#define ERROR_STATUS  0x19
+#define ERROR_RANGE1  0x1a
+#define ERROR_RANGE2  0x1b
+#define ERROR_RANGE3  0x1c
+#define ERROR_SYNC    0x1d
+#define ERROR_MISC    0x1e
+
+#define DIGO_STRENGTH 0x1f
+#define R2_RATE       0x21
+#define R3_RATE_CH1   0x22
+#define R3_RATE_CH2   0x23
+#define R3_RATE_CH3   0x24
+#define R1_RATE       0x25
+#define DIS_EFILTER   0x26
+#define DRDYB_SRC     0x27
+#define SYNCB_CN      0x28
+#define MASK_DRDYB    0x29
+#define MASK_ERB      0x2a
+#define ALARM_FILTER  0x2e
+#define CH_CNFG       0x2f
+#define DATA_STATUS   0x30
+#define DATA_CH1_PACE 0x31
+#define DATA_CH2_PACE 0x33
+#define DATA_CH3_PACE 0x35
+#define DATA_CH1_ECG  0x37
+#define DATA_CH2_ECG  0x3a
+#define DATA_CH3_ECG  0x3d
+#define REVID         0x40
+#define DATA_LOOP     0x50
+
 static inline int32_t convert_channel_data(uint8_t* data)
 {
     uint32_t tmp = ((uint32_t)data[0] << 16) | ((uint32_t)data[1] << 8) | data[2];
@@ -11,25 +71,6 @@ static inline int32_t convert_channel_data(uint8_t* data)
     tmp = (int32_t)tmp;
     tmp >>= 8;
     return tmp;
-}
-
-int32_t ads1293::get_channel_data(uint8_t channel_indx)
-{
-    uint8_t tmp[3];
-    channel_indx *= 3;
-    tmp[0] = read_reg(DATA_CH1_ECG + channel_indx);
-    tmp[1] = read_reg(DATA_CH1_ECG + channel_indx + 1);
-    tmp[2] = read_reg(DATA_CH1_ECG + channel_indx + 2);
-    return convert_channel_data(tmp);
-}
-
-void ads1293::get_channel_data_3chan(int32_t& ch_1, int32_t& ch_2, int32_t& ch_3)
-{
-    uint8_t tmp[9];
-    read_data_stream(tmp, 9);
-    ch_1 = convert_channel_data(tmp);
-    ch_2 = convert_channel_data(tmp + 3);
-    ch_3 = convert_channel_data(tmp + 6);
 }
 
 void ads1293::read_data_stream(uint8_t* data, int length)
@@ -90,25 +131,12 @@ uint8_t ads1293::read_reg(uint8_t addr)
     return (rdData);
 }
 
-bool ads1293::is_data_ready()
-{
-    return !digitalRead(pin_drdy_);
-}
-
 #else /// ARDUINO
 
 #include <inttypes.h>
 #include <vector>
 #include <cmath>
 #include "ads1293.h"
-
-int32_t ads1293::get_channel_data(uint8_t channel_indx)
-{
-    return 0;
-}
-
-void ads1293::get_channel_data_3chan(int32_t& ch_1, int32_t& ch_2, int32_t& ch_3)
-{}
 
 float const ch_coef_ = 0.05;
 float const digital_range_ = 8388608.0; /// 23 bits (2^23, half of the 24 bit range)
@@ -147,11 +175,6 @@ uint8_t ads1293::read_reg(uint8_t addr)
 {
     uint8_t rdData = 0;
     return (rdData);
-}
-
-bool ads1293::is_data_ready()
-{
-    return true;
 }
 
 #endif /// ARDUINO
